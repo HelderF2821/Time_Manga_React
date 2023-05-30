e18013bff3d6f1344062e18fed17af9b86c837fb
HelderF2821 <helder.ferreira2821@gmail.com>
2023-05-30 11:57:22 +0200

    first modif

---
+++ src/MainScreen.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './MainScreen.css'; // Importe le fichier CSS pour cette page
+
+const MainScreen = () => {
+  return (
+    <div className="container">
+      <div className="slidemenu">
+        <header>
+            <img src={require('./logo.png')} alt="Logo" className="logo" />
+        </header>
+        <section className="search-section">
+            <h1>Recherchez un Manga</h1>
+            <div className="search-bar">
+            <input type="text" placeholder="Cherche un Manga" />
+            <img src={require('./settings.png')} alt="Settings" className="settings" />
+            </div>
+        </section>
+        {/* Reste du contenu */}
+        <footer> 
+            {/* Navigation inférieure */}
+        </footer>
+      </div>
+    </div>
+  );
+};
+
+export default MainScreen;
